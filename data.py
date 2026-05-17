d45827a31f23add67b3031a9460b4868d107791b
Muhammad Mamoon Ali <mamoonali926568@gmail.com>
2026-05-17 11:26:44 +0500

    Backend

--- data.py
+++ data.py
@@ -3,9 +3,11 @@ from autogen_ext.models.openai import OpenAICompletionModel
 from autogen_ext.code_executor.docker import DockerCommandLineCodeExecutor
 from autogen_agentchat.teams import RoundRobinGroupChat
 from autogen_agentchat.conditions import TextMentionTermination
+from autogen_agentchat.messages import TextMessage
+from autogen_agentchat.base import TaskResult
 import asyncio
 
-async def main():
+async def teamConfig():
     model= OpenAICompletionModel(
         model="o3-mini",
         api_key=open("key.txt").read().strip()
@@ -20,6 +22,8 @@ async def main():
         You need to analyze the csv file and answer the question in python code based on the data in the csv file.
         You should always begin with your plan to answer the question, and then write the code in a code block with language python specified.
         If you need several code blocks to answer the question, you can write one code blocks at a time.
+        Used pandas to analyze the csv file and answer the question.
+        If library si not installed in the docker, you can install it by pip install in the code block.
         once you have the code exceution result, you should provifde the final result
         after that you should exactly say "Terminate" to end the conversation."""
         ),
@@ -32,11 +36,36 @@ async def main():
     executor= CodeExecutorAgent(
         name='CodeExecutor',
         code_executor=docker,
-
     )
 
     team= RoundRobinGroupChat(
         participants=[developer, executor],
         termination_condition=TextMentionTermination("Terminate"),
         max_turns=10,
-    )
+    )   
+    return team, docker
+
+
+async def orchestrate(team, docker, task):
+    await docker.start()
+    async for message in team.run_stream():
+        if isinstance(message, TextMessage):
+            print(message:=f"{message.source}: {message.content}")
+            yield message
+        elif isinstance(message, TaskResult):
+            print(message:=f"Stop reason: {message.stop_reason}")
+            yield message
+
+    await docker.stop()
+
+async def main():
+        task="My dataset is Data.csv, How many columns are in it?"
+        team, docker= await teamConfig()
+        async for message in orchestrate(team, docker, task):
+            pass
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+       
